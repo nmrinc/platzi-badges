346c58e30cc7f8971ef60ce1e049825d4cbf0e21
Erick Avila <nmr.inc@icloud.com>
2021-02-05 12:29:09 -0600

    Class 6

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
+//@concept ReactDOM.render functionality
 
-// const container = document.getElementById('app');
+/**
+ * @context This is the way html would do it
+ * @tst const element = document.createElement('h1');
+ * @tst element.innerText = 'Hello, Platzi Badges!';
 
-// container.appendChild(element);
+ * @tst const container = document.getElementById('app');
 
+ * @tst container.appendChild(element);
+*/
+
+//@context This way it's using React
+//@a Import react dependencies.
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+//@o This is JSX syntax. And every time you want to use it it's necessary to import react
+const element = <h1>Hello, Platzi Badges from React!</h1>;
 
 const container = document.getElementById('app');
 
-// ReactDOM.render(__qué__, __dónde__);
+//@o ReactDOM.render(__qué__, __dónde__);
 ReactDOM.render(element, container);
