d445794735448af9eb8c003e8147faf8eff7234c
Erick Avila <nmr.inc@icloud.com>
2021-02-16 20:51:05 -0600

    Comments added to BadgeEdit.js about auto refresh when an update on the data occurs

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -10,70 +10,79 @@ import MiniLoader from '../components/MiniLoader';
 import api from '../api';
 
 class Badges extends React.Component {
-  state = {
-    loading: true,
-    error: null,
-    data: undefined,
-  };
+	state = {
+		loading: true,
+		error: null,
+		data: undefined,
+	};
 
-  componentDidMount() {
-    this.fetchData();
+	componentDidMount() {
+		this.fetchData();
 
-    this.intervalId = setInterval(this.fetchData, 5000);
-  }
+		//@concept POLLING : simply hitting an API’s endpoint to retrieve any new data at a set interval of time.
+		//! It's not a good practice because will generate an unwanted calls and overload traffic to the API. An option would be used Webhooks.
+		//@o For practical use to exemplify the process of refreshing the data, it will be used in this class.
+		//@a An easy approach would be use a setInterval with a given time to run the fetch function.
+		this.intervalId = setInterval(this.fetchData, 5000);
+	}
 
-  componentWillUnmount() {
-    clearInterval(this.intervalId);
-  }
+	//@o As the component it's unmounted, we need to clear the interval with a given id.
+	componentWillUnmount() {
+		clearInterval(this.intervalId);
+	}
 
-  fetchData = async () => {
-    this.setState({ loading: true, error: null });
+	fetchData = async () => {
+		this.setState({ loading: true, error: null });
 
-    try {
-      const data = await api.badges.list();
-      this.setState({ loading: false, data: data });
-    } catch (error) {
-      this.setState({ loading: false, error: error });
-    }
-  };
+		try {
+			const data = await api.badges.list();
+			this.setState({ loading: false, data: data });
+		} catch (error) {
+			this.setState({ loading: false, error: error });
+		}
+	};
 
-  render() {
-    if (this.state.loading === true && !this.state.data) {
-      return <PageLoading />;
-    }
+	render() {
+		//@o As every 5 secs the fetch function will be invoked, it's bad for the UX to block the SPA with the loader, so we invoke it only at the beginning when there's no data
+		if (this.state.loading === true && !this.state.data) {
+			return <PageLoading />;
+		}
 
-    if (this.state.error) {
-      return <PageError error={this.state.error} />;
-    }
+		if (this.state.error) {
+			return <PageError error={this.state.error} />;
+		}
 
-    return (
-      <React.Fragment>
-        <div className="Badges">
-          <div className="Badges__hero">
-            <div className="Badges__container">
-              <img
-                className="Badges_conf-logo"
-                src={confLogo}
-                alt="Conf Logo"
-              />
-            </div>
-          </div>
-        </div>
+		return (
+			<React.Fragment>
+				<div className="Badges">
+					<div className="Badges__hero">
+						<div className="Badges__container">
+							<img
+								className="Badges_conf-logo"
+								src={confLogo}
+								alt="Conf Logo"
+							/>
+						</div>
+					</div>
+				</div>
 
-        <div className="Badges__container">
-          <div className="Badges__buttons">
-            <Link to="/badges/new" className="btn btn-primary">
-              New Badge
-            </Link>
-          </div>
+				<div className="Badges__container">
+					<div className="Badges__buttons">
+						<Link to="/badges/new" className="btn btn-primary">
+							New Badge
+						</Link>
+					</div>
 
-          <BadgesList badges={this.state.data} />
+					<BadgesList badges={this.state.data} />
 
-          {this.state.loading && <MiniLoader />}
-        </div>
-      </React.Fragment>
-    );
-  }
+					{
+						//@o For a good UX it's a good practice to let the user know that there's a process running, so adding a mini loader will fix this.
+						this.state.loading && <MiniLoader />
+					}
+				</div>
+			</React.Fragment>
+		);
+	}
 }
 
 export default Badges;
