e5511353fee56e7ce993d752bc709a037a8356de
Erick Avila <nmr.inc@icloud.com>
2021-02-17 20:39:29 -0600

    Comments added from classes

--- src/pages/BadgeEdit.js
+++ src/pages/BadgeEdit.js
@@ -8,99 +8,102 @@ import PageLoading from '../components/PageLoading';
 import api from '../api';
 
 class BadgeEdit extends React.Component {
-  state = {
-    loading: true,
-    error: null,
-    form: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      jobTitle: '',
-      twitter: '',
-    },
-  };
+	state = {
+		loading: true,
+		error: null,
+		form: {
+			firstName: '',
+			lastName: '',
+			email: '',
+			jobTitle: '',
+			twitter: '',
+		},
+	};
 
-  componentDidMount() {
-    this.fetchData();
-  }
+	//@o As this page will need to get the data from the user, we need to fetch it from the api with the given id
+	componentDidMount() {
+		this.fetchData();
+	}
 
-  fetchData = async e => {
-    this.setState({ loading: true, error: null });
+	fetchData = async (e) => {
+		this.setState({ loading: true, error: null });
 
-    try {
-      const data = await api.badges.read(this.props.match.params.badgeId);
+		try {
+			//@o To obtain props from the url we use the .match on the object .params from the router
+			const data = await api.badges.read(this.props.match.params.badgeId);
 
-      this.setState({ loading: false, form: data });
-    } catch (error) {
-      this.setState({ loading: false, error: error });
-    }
-  };
+			//@o As we're going to consume the data into the form, we pass this data into the state.form
+			this.setState({ loading: false, form: data });
+		} catch (error) {
+			this.setState({ loading: false, error: error });
+		}
+	};
 
-  handleChange = e => {
-    this.setState({
-      form: {
-        ...this.state.form,
-        [e.target.name]: e.target.value,
-      },
-    });
-  };
+	handleChange = (e) => {
+		this.setState({
+			form: {
+				...this.state.form,
+				[e.target.name]: e.target.value,
+			},
+		});
+	};
 
-  handleSubmit = async e => {
-    e.preventDefault();
-    this.setState({ loading: true, error: null });
+	handleSubmit = async (e) => {
+		e.preventDefault();
+		this.setState({ loading: true, error: null });
 
-    try {
-      await api.badges.update(this.props.match.params.badgeId, this.state.form);
-      this.setState({ loading: false });
+		try {
+			await api.badges.update(this.props.match.params.badgeId, this.state.form);
+			this.setState({ loading: false });
 
-      this.props.history.push('/badges');
-    } catch (error) {
-      this.setState({ loading: false, error: error });
-    }
-  };
+			this.props.history.push('/badges');
+		} catch (error) {
+			this.setState({ loading: false, error: error });
+		}
+	};
 
-  render() {
-    if (this.state.loading) {
-      return <PageLoading />;
-    }
+	render() {
+		if (this.state.loading) {
+			return <PageLoading />;
+		}
 
-    return (
-      <React.Fragment>
-        <div className="BadgeEdit__hero">
-          <img
-            className="BadgeEdit__hero-image img-fluid"
-            src={header}
-            alt="Logo"
-          />
-        </div>
+		return (
+			<React.Fragment>
+				<div className="BadgeEdit__hero">
+					<img
+						className="BadgeEdit__hero-image img-fluid"
+						src={header}
+						alt="Logo"
+					/>
+				</div>
 
-        <div className="container">
-          <div className="row">
-            <div className="col-6">
-              <Badge
-                firstName={this.state.form.firstName || 'FIRST_NAME'}
-                lastName={this.state.form.lastName || 'LAST_NAME'}
-                twitter={this.state.form.twitter || 'twitter'}
-                jobTitle={this.state.form.jobTitle || 'JOB_TITLE'}
-                email={this.state.form.email || 'EMAIL'}
-                avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
-              />
-            </div>
+				<div className="container">
+					<div className="row">
+						<div className="col-6">
+							<Badge
+								firstName={this.state.form.firstName || 'FIRST_NAME'}
+								lastName={this.state.form.lastName || 'LAST_NAME'}
+								twitter={this.state.form.twitter || 'twitter'}
+								jobTitle={this.state.form.jobTitle || 'JOB_TITLE'}
+								email={this.state.form.email || 'EMAIL'}
+								avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
+							/>
+						</div>
 
-            <div className="col-6">
-              <h1>Edit Attendant</h1>
-              <BadgeForm
-                onChange={this.handleChange}
-                onSubmit={this.handleSubmit}
-                formValues={this.state.form}
-                error={this.state.error}
-              />
-            </div>
-          </div>
-        </div>
-      </React.Fragment>
-    );
-  }
+						<div className="col-6">
+							<h1>Edit Attendant</h1>
+							<BadgeForm
+								onChange={this.handleChange}
+								onSubmit={this.handleSubmit}
+								formValues={this.state.form}
+								error={this.state.error}
+							/>
+						</div>
+					</div>
+				</div>
+			</React.Fragment>
+		);
+	}
 }
 
 export default BadgeEdit;
